26311b1cde2de71b8a35d0f3f80d81b3603c3a7c
Shamil Kamaev <shamilkamaev@MacBook-Air-Shamil.local>
2022-03-20 22:37:08 +0400

    svg plaugin added

--- src/components/SkillCard/SkillCard.vue
+++ src/components/SkillCard/SkillCard.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="SkillCard">
     <div class="title">Skill card title</div>
+
     <ProgressBar :progress="progress" />
-    <AwesomeIcon name="plus" />
+
+    <button class="handle-button">
+      <PuaseIcon class="handle-icon" />
+    </button>
+
+    <button class="handle-button">
+      <PlayIcon class="handle-icon" />
+    </button>
+
+    <button class="handle-button">
+      <StopIcon class="handle-icon" /> 
+    </button>
   </div>
 </template>
 
@@ -10,10 +22,14 @@
   import { onMounted, ref } from 'vue'
   import ProgressBar from '~/components/ProgressBar'
 
-  const progress = ref(0)
+  import PuaseIcon from '~/assets/images/svg/pause.svg'
+  import PlayIcon from '~/assets/images/svg/play.svg'
+  import StopIcon from '~/assets/images/svg/stop.svg'
+
+  const progress = ref<number>(0)
 
   onMounted(() => {
-    setInterval(() => {
+    setInterval(():void => {
       if (progress.value < 100)
       progress.value ++
     }, 2000)
