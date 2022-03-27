f4a7766d1db49826a665dcd87413178b8a5ef4e5
Shamil Kamaev <shamilkamaev@MacBook-Air-Shamil.local>
2022-03-27 14:58:23 +0400

    added time input

--- src/components/ProgressBar/ProgressBar.vue
+++ src/components/ProgressBar/ProgressBar.vue
@@ -5,12 +5,16 @@
   >
     <div
       class="progress-scale"
+      :class="{'progress-scale--isRuning': isRuning}"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-  const props = defineProps<{ progress: number }>()
+  const props = defineProps<{
+    progress: number,
+    isRuning: boolean,
+  }>()
 </script>
 
 <style lang="scss" src="./ProgressBar.scss" scoped />
--- src/components/SkillCard/SkillCard.vue
+++ src/components/SkillCard/SkillCard.vue
@@ -2,24 +2,30 @@
   <div class="SkillCard">
     <div class="title">Skill card title</div>
 
-    <ProgressBar :progress="timeLeftInPercents" />
+    <ProgressBar
+      :progress="timeLeftInPercents"
+      :is-runing="isRuning"
+    />
 
     <div class="handle-buttons">
-      <button class="handle-button">
-        <PuaseIcon class="handle-icon" />
+      <button
+        class="handle-button"
+        @click="playPauseHandle"
+      >
+        <PuaseIcon v-if="isRuning" class="handle-icon" />
+        <PlayIcon v-else class="handle-icon" />
       </button>
 
-      <button class="handle-button">
-        <PlayIcon class="handle-icon" />
-      </button>
-
-      <button class="handle-button">
+      <button
+        class="handle-button"
+        @click="stopCounter"
+      >
         <StopIcon class="handle-icon" /> 
       </button>
     </div>
 
     <input
-      v-model="timeRange"
+      v-model.number="timeRange"
       type="range"
       min="0"
       max="100"
@@ -42,22 +48,40 @@
   import StopIcon from '~/assets/images/svg/stop.svg'
 
   const timeRange = ref<number>(1),
-        elapsedSeconds = ref<number>(0)
+        elapsedSeconds = ref<number>(0),
+        isRuning = ref<boolean>(false)
+
+  let counterInterval: ReturnType<typeof setInterval>
   
-  const timeLeftInPercents = computed(() => {
-    const timeRangeInSeconds = timeRange.value * 3600
-    const timeLeftInSeconds = timeRangeInSeconds - elapsedSeconds.value
+  
+  const timeLeftInPercents = computed(():number => {
+    const timeRangeInSeconds: number = timeRange.value * 3600
+    const timeLeftInSeconds: number = timeRangeInSeconds - elapsedSeconds.value
     return Number((100 - (timeLeftInSeconds / timeRangeInSeconds * 100)).toFixed(2))
   })
 
-
-
-  onMounted(() => {
-    setInterval(():void => {
-      if (timeLeftInPercents.value < 100)
+  const runCounter = ():void => {
+    isRuning.value = true
+    counterInterval = setInterval(():void => {
+      if (timeLeftInPercents.value >= 100) return clearInterval(counterInterval)
       elapsedSeconds.value ++
     }, 1000)
-  })
+  }
+
+  const pauseCounter = ():void => {
+    isRuning.value = false
+    clearInterval(counterInterval)
+  }
+
+  const stopCounter = ():void => {
+    elapsedSeconds.value = 0
+    isRuning.value = false
+    clearInterval(counterInterval)
+  }
+
+  const playPauseHandle = ():void => {
+    isRuning.value ? pauseCounter() : runCounter()
+  }
 
 </script>
 
