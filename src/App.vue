1d66b18ed73810001f94c2ebf38e3f8ce158d1ec
Shamil Kamaev <shamilkamaev@MacBook-Air-Shamil.local>
2022-05-21 12:31:25 +0400

    rewind handlers added

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,33 @@
 </template>
 
 <script setup lang="ts">
+  import { onMounted, ref } from 'vue'
   import SkillCard from '~/components/SkillCard'
+
+  let SkillCardsList = ref<Object>({})
+
+  onMounted(() => {
+    initDataFromLocalStorage()
+  })
+
+
+  const initDataFromLocalStorage = ():void => {
+    const localStorage = window.localStorage
+
+    localStorage.setItem('cardsList', JSON.stringify(
+      {
+        'kdslkm1221laskl': {
+          title: 'Skill card title',
+          elapsedSeconds: 31,
+          timeRange: 1,
+        }
+      }
+    ))
+
+    SkillCardsList = JSON.parse(localStorage.getItem('cardsList') || '{}')
+
+    console.log(SkillCardsList)
+  }
 </script>
 
 <style>
--- src/components/SkillCard/SkillCard.vue
+++ src/components/SkillCard/SkillCard.vue
@@ -10,10 +10,20 @@
     <div class="handle-buttons">
       <button
         class="handle-button"
+        @click="fastBackward"
+      >
+        <FastBackwardIcon class="handle-icon" />
+      </button>
+
+      <button
+        class="handle-button"
+        :disabled="timeIsUp"
         @click="playPauseHandle"
       >
-        <PuaseIcon v-if="isRuning" class="handle-icon" />
-        <PlayIcon v-else class="handle-icon" />
+        <component
+          :is="isRuning ? PuaseIcon : PlayIcon"
+          class="handle-icon"
+        />
       </button>
 
       <button
@@ -22,18 +32,30 @@
       >
         <StopIcon class="handle-icon" /> 
       </button>
+
+      <button
+        class="handle-button"
+        :disabled="timeIsUp"
+        @click="fastForward"
+      >
+        <FastForwardIcon class="handle-icon" /> 
+      </button>
     </div>
 
     <input
       v-model.number="timeRange"
       type="range"
-      min="0"
+      min="1"
       max="100"
       step="1"
     />
 
     <div class="time-range-container">
       {{ timeRange }}
+    </div>
+
+    <div>
+      {{ timeLefInHHMMSS }}
     </div>
     
   </div>
@@ -46,10 +68,10 @@
   import PuaseIcon from '~/assets/images/svg/pause.svg'
   import PlayIcon from '~/assets/images/svg/play.svg'
   import StopIcon from '~/assets/images/svg/stop.svg'
+  import FastForwardIcon from '~/assets/images/svg/fast-forward.svg'
+  import FastBackwardIcon from '~/assets/images/svg/fast-backward.svg'
 
-  const timeRange = ref<number>(1),
-        elapsedSeconds = ref<number>(0),
-        isRuning = ref<boolean>(false)
+  const [timeRange, elapsedSeconds, isRuning] = [ref<number>(1), ref<number>(0), ref<boolean>(false)]
 
   let counterInterval: ReturnType<typeof setInterval>
   
@@ -59,6 +81,18 @@
     const timeLeftInSeconds: number = timeRangeInSeconds - elapsedSeconds.value
     return Number((100 - (timeLeftInSeconds / timeRangeInSeconds * 100)).toFixed(2))
   })
+
+  const timeRangeInSeconds = computed(():number => {
+    return timeRange.value * 3600
+  })
+
+  const timeLefInHHMMSS = computed(():string =>
+    new Date(elapsedSeconds.value * 1000).toISOString().substring(11, 19)
+  )
+
+  const timeIsUp = computed(():boolean =>
+    elapsedSeconds === timeRangeInSeconds
+  )
 
   const runCounter = ():void => {
     isRuning.value = true
@@ -81,6 +115,20 @@
 
   const playPauseHandle = ():void => {
     isRuning.value ? pauseCounter() : runCounter()
+  }
+
+  const fastBackward = ():void | number => {
+    if (elapsedSeconds.value < 10) return elapsedSeconds.value = 0
+    elapsedSeconds.value = elapsedSeconds.value - 10
+  }
+
+  const fastForward = ():void | boolean => {
+    if ((elapsedSeconds.value + 10) >= timeRangeInSeconds.value) {
+      elapsedSeconds.value = timeRangeInSeconds.value
+      clearInterval(counterInterval)
+      return isRuning.value = false
+    }
+    elapsedSeconds.value = elapsedSeconds.value + 10
   }
 
 </script>
